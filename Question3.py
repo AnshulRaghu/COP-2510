9ba33969a3f9d761719b64fc4e264f8888d0bd39
Anshul Raghuvanshi <65981241+AnshulRaghu@users.noreply.github.com>
2022-10-14 11:14:10 -0400

    Add files via upload

---
+++ Question3.py
@@ -0,0 +1,30 @@
+# Driver: Christian Taylor          U-Number: U75799592     Participation Percentage: 50%
+# Navigator: Anshul Raghuvanshi     U-Number: U69656337     Participation Percentage: 50%
+
+# This program allows us to calculate the amount of the tip given the tip amount
+# percentage and the subtotal.
+
+# Use the input function to prompt the user to assign a value of type float to two new
+# variables "subtotal" and "tipAmount".
+
+subtotal = float(input('Please enter the subtotal: '))
+tipAmount = float(input('Now enter the tip amount (as a %): '))
+
+# Create a new variable called "tip". In order to calculate the tip, you take the percentage
+# amount and divide it by 100 then multiply the result by the subtotal.
+# Create another variable "total". The sum of the subtotal and the tip gives you the total.
+
+tip = (tipAmount/100) * subtotal
+total = subtotal + tip
+
+# Use the format function inside of the print function to adjust the number of decimal places
+# to two and to format as a floating point value.
+# Use sep="" to remove the space between the dollar sign and the numerical value.
+
+print('Subtotal: $', format(subtotal, ",.2f"), sep="")
+print('Tip: $', format(tip, ",.2f"), sep="")
+print('Total: $', format(total, ",.2f"), sep="")
+
+
+
+
---
+++ main.py
@@ -0,0 +1,11 @@
+# Driver: Anshul Raghuvanshi (U69656337), Navigator: Christian Taylor (U75799592)
+# This program is intended to calculate the area of a hexagon using the user's input.
+# We calculated this by the following formula: ((3√3)/2)*(s^2)
+
+import math
+
+s = float(input("\nEnter the side length of the hexagon: "))
+b = (3 * math.sqrt(3)) / 2
+z = b * (s * s)
+
+print('The area of the hexagon with side length', s, 'is', round(z, 3))
